4c9d4b05ae7f2b21f2f93fca8779fa4abc334c2a
sduvdev <134515395+sduvdev@users.noreply.github.com>
2023-12-19 22:30:01 +0200

    a new controller for "customers" requests +  update for the  server main file. (#4)
    Co-authored-by: sduvdev <shduvdev@gmail.com>

--- server.js
+++ server.js
@@ -1,47 +1,46 @@
 const express = require("express");
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
-
-const coursesController = require("./controllers/coursesController")
-const filesController = require("./controllers/filesopreationcontroler")
+const coursesController = require("./controllers/coursesController");
+const filesController = require("./controllers/filesopreationcontroler");
 const classController = require("./controllers/ClassController");
 const candidates = require("./controllers/CandidateController");
-const users = require("./controllers/usersController.js")
-const sorting_days = require("./controllers/sortingDayController")
-
+const users = require("./controllers/usersController");
+const sorting_days = require("./controllers/sortingDayController");
+const customer = require("./controllers/customerController");
 
 const app = express();
 app.use(cors());
-app.use(bodyParser.json({ limit: '10mb' }));
-app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
+app.use(bodyParser.json({ limit: "10mb" }));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
 
+app.use("/class", classController);
+app.use("/courses", coursesController);
+app.use("/files", filesController);
+app.use("/candidate", candidates);
+app.use("/users", users);
+app.use("/sorting_day", sorting_days);
+app.use("/customers", customer);
 
-app.use('/class',classController);
-app.use('/courses',coursesController);
-app.use('/files',filesController);
-app.use('/candidate',candidates)
-app.use('/users',users)
-app.use('/sorting_day', sorting_days)
-
-app.get('/getfile/:filename', (req, res) => {
-    let filename = req.params.filename
-    const encoded = encodeURIComponent(filename);
-    app.use(
-        `/down/${encoded}`, 
-        express.static(`./files/${req.params.filename}`,
-        {setHeaders: (res) => {
-            res.setHeader('Content-Disposition', `attachment; filename*=UTF-8''${encoded}`);
-        }
+app.get("/getfile/:filename", (req, res) => {
+  let filename = req.params.filename;
+  const encoded = encodeURIComponent(filename);
+  app.use(
+    `/down/${encoded}`,
+    express.static(`./files/${req.params.filename}`, {
+      setHeaders: (res) => {
+        res.setHeader(
+          "Content-Disposition",
+          `attachment; filename*=UTF-8''${encoded}`
+        );
+      },
     })
-    )
-    res.send(`/down/${encoded}`)
-}  
-
-)
-
+  );
+  res.send(`/down/${encoded}`);
+});
 
 const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
-    console.log(`server is run in ${PORT}`)
-})
+  console.log(`server is run in ${PORT}`);
+});
