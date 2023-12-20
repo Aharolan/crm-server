a3affbbf1d2489a6dce343bd8668b31d198ca7fd
binyamin148 <benibur2079@gmail.com>
2023-12-20 12:28:02 +0200

    I had a problem with the database and I recreated it and the name of the path in the baseserver changed

--- baseServer.js
+++ baseServer.js
@@ -1,5 +1,5 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./student_sqlite/candidate_list.db');
+const db = new sqlite3.Database('./student.sqlite/candidate_list.db');
 
 
 const get = (collection) => {
