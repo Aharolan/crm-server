8fb1ff939fe9cbb1a1b92587cd5fdc843999d5a1
Aharolan <a0548532272@gmail.com>
2024-01-16 17:17:31 +0200

    rebase team_b

--- controllers/InterviewsController.js
+++ controllers/InterviewsController.js
@@ -2,31 +2,6 @@ const express = require("express");
 const router = express.Router();
 const db = require("../dataBase");
 
-const getInterviews = () => {
-  return new Promise((resolve, reject) => {
-    const query = `
-    SELECT
-    i.position,
-    DATE_FORMAT(i.date, '%d/%m/%y') AS date,
-    c.company_name,
-    i.interview_id as id
-  FROM 
-    customers c,
-      interviews i
-  WHERE 
-    c.customer_id = i.customer_id
-  ORDER BY date`;
-    db.query(query, [], (err, rows) => {
-      if (err) {
-        console.error("Error fetching data:", err);
-        reject(err);
-      } else {
-        resolve(rows);
-      }
-    });
-  });
-};
-const db = require("../dataBase");
 const { getColumn, add, updateData, getRows } = require("../baseServer");
 
 const getColumnInfo = async (req, res) => {
@@ -56,7 +31,6 @@ const getInterviews = () => {
 
 const getAll = async (req, res) => {
   const resArray = await getInterviews();
-  const resArray = await getInterviews();
   try {
     res.status(200).send(resArray);
   } catch (error) {
@@ -64,22 +38,12 @@ const getAll = async (req, res) => {
   }
 };
 
-
 const getCompanyInterviews = (id) => {
   return new Promise((resolve, reject) => {
-    const query = `
-    SELECT
-    i.position,
-    DATE_FORMAT(i.date, '%d/%m/%y') AS date,
-    c.company_name,
-    i.interview_id as id
-  FROM 
-    customers c,
-      interviews i
-  WHERE 
-    c.customer_id = i.customer_id
-    and 
-    c.customer_id = ?
+    const query = 
+  `SELECT i.position, DATE_FORMAT(i.date, '%d/%m/%y') AS date, c.company_name, i.interview_id as id
+  FROM customers c, interviews i
+  WHERE c.customer_id = i.customer_id and c.customer_id = ?
   ORDER BY date`;
     db.query(query, [id], (err, rows) => {
       console.log(query)
@@ -150,7 +114,6 @@ const addInterview = async (request, response) => {
 
 router.get("/", getAll);
 router.get("/read/:id", getByCompany);
-
 router.get("/column/:columnName", getColumnInfo);
 router.post("/create", addInterview);
 module.exports = router;
