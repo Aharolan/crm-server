0bfa5dc0339ab6d7c20ee467bfb46cf7cb291df7
Aharolan <a0548532272@gmail.com>
2024-01-30 12:42:35 +0200

    fix according to the PR requests

--- controllers/InterviewsController.js
+++ controllers/InterviewsController.js
@@ -5,7 +5,7 @@ const {add, delete_, updateData} = require("../baseServer");
 
 const getInterviews = () => {
   return new Promise((resolve, reject) => {
-    const query = `SELECT i.position, DATE_FORMAT(i.date, '%d/%m/%y') AS date, c.company_name, i.interview_id as id
+    const query = `SELECT i.position, DATE_FORMAT(i.date, '%d/%m/%y') AS date, c.company_name as companyName, i.interview_id as id
   FROM customers c, interviews i
   WHERE  c.customer_id = i.customer_id ORDER BY date`;
     db.query(query, [], (err, rows) => {
@@ -31,12 +31,11 @@ const getAll = async (req, res) => {
 const getCompanyInterviews = (id) => {
   return new Promise((resolve, reject) => {
     const query = 
-  `SELECT i.position, DATE_FORMAT(i.date, '%d/%m/%y') AS date, c.company_name, i.interview_id as id
+  `SELECT i.position, DATE_FORMAT(i.date, '%d/%m/%y') AS date, c.company_name as companyName, i.interview_id as id
   FROM customers c, interviews i
   WHERE c.customer_id = i.customer_id and c.customer_id = ?
   ORDER BY date`;
     db.query(query, [id], (err, rows) => {
-      console.log(query)
       if (err) {
         console.error("Error fetching data:", err);
         reject(err);
@@ -56,60 +55,94 @@ const getByCompany = async (req, res) => {
   }
 };
 
+const deleteOperation = async (tableName, ID, columnName) => {
+  try {
+    delete_(tableName, ID, columnName);
+  } catch (error) {
+    console.error(`Error deleting from ${tableName}:`, error);
+    throw error;
+  }
+};
+
+const updateOperation = async (tableName, updatedData, ID, IDValue) => {
+  try {
+    updateData(tableName, updatedData, ID, IDValue);
+  } catch (error) {
+    console.error(`Error updating ${tableName}:`, error);
+    throw error;
+  }
+};
+
+const addOperation = async (tableName, data) => {
+  try {
+    const ID = await add(tableName, data);
+    return ID;
+  } catch (error) {
+    console.error(`Error adding to ${tableName}:`, error);
+    throw error;
+  }
+};
+
 const editInterview = async (request, response) => {
   try {
-    const interviewID = await request.params.id;
-    delete_("graduates_interviews", interviewID, "interview_id");
-    delete_("interviews_technologies", interviewID, "interview_id");
-    updateData( "interviews", request.body.updateInterview, "interview_id", interviewID)
-    request.body.students_id.map((students) => {
-      add("graduates_interviews", {student_id: students, interview_id: interviewID})});
-    request.body.technology_id.map((technology) => {
-      add("interviews_technologies", {technology_id: technology, interview_id: interviewID})});
-    for (let technology of request.body.newTechnologies) {
-      const newTechID = await add("technologies", {name: technology});
-      await add("interviews_technologies", {technology_id: newTechID, interview_id: interviewID});
+    const interviewID = request.params.id;
+    const { updateInterview, students_id, technology_id, newTechnologiesNames } = request.body;
+
+    await deleteOperation("graduates_interviews", interviewID, "interview_id");
+    await deleteOperation("interviews_technologies", interviewID, "interview_id");
+
+    await updateOperation("interviews", updateInterview, "interview_id", interviewID);
+
+    await Promise.all(students_id.map(student => addOperation("graduates_interviews", { student_id: student, interview_id: interviewID })));
+    await Promise.all(technology_id.map(tech => addOperation("interviews_technologies", { technology_id: tech, interview_id: interviewID })));
+
+    for (let technology of newTechnologiesNames) {
+      const newTechID = await addOperation("technologies", { name: technology });
+      await addOperation("interviews_technologies", { technology_id: newTechID, interview_id: interviewID });
     }
+
     response.status(200).send({ message: "interview updated" });
   } catch (error) {
     response.status(500).send(error);
   }
 };
 
+
+
 const infoToEditInterview = (id) => {
   return new Promise((resolve, reject) => {
-    const query1 = `
+    const candidatesInfoQuery = `
       SELECT
       CONCAT(s.last_name, ' ', s.first_name) AS name,
         s.student_id as id,
-        right(c.name, 1) as class_number,
+        right(c.name, 1) as classNumber,
         CASE WHEN gi.student_id IS NOT NULL THEN TRUE ELSE FALSE END as isSelected
       FROM students s
       JOIN classes c ON s.class_id = c.class_id
-      LEFT JOIN graduates_interviews gi ON s.student_id = gi.student_id AND gi.interview_id = ${id}
+      LEFT JOIN graduates_interviews gi ON s.student_id = gi.student_id AND gi.interview_id = ?
       WHERE s.student_status_id = 2
-      ORDER BY s.last_name
+      ORDER BY isSelected DESC
     `;
-    db.query(query1, [], (err1, candidates) => {
+    db.query(candidatesInfoQuery, [id], (err1, candidates) => {
       if (err1) {
-        console.error("Error fetching data from query1:", err1);
+        console.error("Error fetching data from candidatesInfoQuery:", err1);
         reject(err1);
       } else {
-        const query2 = `
+        const technologiesInfoQuery = `
           SELECT 
             t.technology_id as id,
             t.name,
             CASE WHEN it.technology_id IS NOT NULL THEN TRUE ELSE FALSE END as isSelected
           FROM technologies t
-          LEFT JOIN interviews_technologies it ON t.technology_id = it.technology_id AND it.interview_id = ${id}
+          LEFT JOIN interviews_technologies it ON t.technology_id = it.technology_id AND it.interview_id = ?
         `;
 
-        db.query(query2, [], (err2, technologies) => {
+        db.query(technologiesInfoQuery, [id], (err2, technologies) => {
           if (err2) {
-            console.error("Error fetching data from query2:", err2);
+            console.error("Error fetching data from technologiesInfoQuery:", err2);
             reject(err2);
           } else {
-            resolve({ candidates: candidates, technologies: technologies }); // Combine results if needed
+            resolve({ candidates: candidates, technologies: technologies }); // Combine results as needed
           }
         });
       }
@@ -129,5 +162,5 @@ const getInterview = async (req, res) => {
 router.get("/", getAll);
 router.get("/read/:id", getByCompany);
 router.put("/update/:id", editInterview);
-router.get("/editInterview/:id", getInterview)
+router.get("/edit_interview/:id", getInterview)
 module.exports = router;
